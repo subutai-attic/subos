af446b16c4093e674fcef1f64a14279fd1b3fd8b
tzununbekov <tzununbekov@critical-factor.com>
2016-02-05 18:15:12 +0600

    Subutai binaries defined, autobuild and agent updated

--- autobuild.sh
+++ autobuild.sh
@@ -164,11 +164,11 @@ if [ "$VM" == "true" -o "$EXPORT" != "false" ]; then
 		echo "Exported to $EXPORT_DIR"
 	fi
 	if [ "$VM" == "true" ]; then
-		echo "Starting vm"
 		vboxmanage startvm --type headless $CLONE
+		echo "Waiting for Subutai IP address"
+		echo -e "Please use following command to access your new Subutai:\\n ssh root@`nc -l 48723`"
 	else
 		vboxmanage unregistervm --delete $CLONE
 	fi
 fi
 
-echo "Please use following IP for access your new RH: `nc -l 48723`"
