4cf46716414f3128854494f1b404c51f80a5fca4
Dmitry Shihovtsev <soffokulus@gmail.com>
2016-02-19 21:50:20 +0600

    Removed autopilot

--- prepare-server.sh
+++ prepare-server.sh
@@ -15,12 +15,12 @@ else
 fi
 /apps/subutai/current/bin/btrfsinit $disk
 
-#systemctl stop snappy-autopilot.service
-#systemctl disable snappy-autopilot.service
-#systemctl stop snappy-autopilot.timer
-#systemctl disable snappy-autopilot.timer
-#mount -o remount,rw /
-#rm -rf /lib/systemd/system/snappy-autopilot.timer
+systemctl stop snappy-autopilot.service
+systemctl disable snappy-autopilot.service
+systemctl stop snappy-autopilot.timer
+systemctl disable snappy-autopilot.timer
+mount -o remount,rw /
+rm -rf /lib/systemd/system/snappy-autopilot.timer
 
 echo "ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEA6NF8iallvQVp22WDkTkyrtvp9eWW6A8YVr+kz4TjGYe7gHzIw+niNltGEFHzD8+v1I2YJ6oXevct1YeS0o9HZyN1Q9qgCgzUFtdOKLv6IedplqoPkcmF0aYet2PkEDo3MlTBckFXPITAMzF8dJSIFo9D8HfdOV0IAdx4O7PtixWKn5y2hMNG0zQPyUecp4pzC6kivAIhyfHilFR61RGL+GPXQ2MWZWFYbAGjyiYJnAmCP3NOTd0jMZEnDkbUvxhMmBYSdETk1rRgm+R4LOzFUGaHqHDLKLX+FIPKcF96hrucXzcWyLbIbEgE98OHlnVYCzRdK8jlqm8tehUc9c9WhQ== vagrant insecure public key" > /home/ubuntu/.ssh/authorized_keys
 ssh-keygen -b 2048 -t rsa -f /root/.ssh/id_rsa -q -N ""
